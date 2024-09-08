94d877c160fe938b88ab9c7a15647014303f5df1
Bianca-Granata <biancag02@outlook.com>
2024-09-07 23:03:54 -0700

    finished simulation of writer with writers block

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,24 +1,21 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Bianca Granata
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * working on the Function calls examples.
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
+function setup() {
+    // happens immediately when starting the program, at once
+    createCanvas(640, 480); //will create a canvas on a page where we can draw stuff using p5
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    // every frame that the program is on how it works with time
+    background(255, 100, 100); //it starts with the name of a function and then the arguments
+    rect(200, 80, 240, 320);
 }
