3d0dc6ccaefb6ad17e9c6b4596325b4d204aa678
Bianca-Granata <biancag02@outlook.com>
2024-09-08 15:51:04 -0700

    created a record

--- topics/Drawing/js/script.js
+++ topics/Drawing/js/script.js
@@ -1,24 +1,45 @@
 /**
- * Title of Project
- * Author Name
+ * The greatest record of all time
+ * Bianca Granata
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * record
+ * practicing and learning drawing functions in p5
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * creates a square canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * displays the record
 */
 function draw() {
+    //grey background
+    background(150);
+    //main part of the record
+    push();
+    fill('black');
+    stroke(255);
+    ellipse(320, 320, 480, 480);
+    pop();
+
+    //label of the record
+    push();
+    fill('red');
+    noStroke();
+    ellipse(320, 320, 140, 140);
+    pop();
+    //hole of the record
+    push();
+    fill('grey');
+    noStroke();
+    ellipse(320, 320, 20);
+    pop();
+
 
 }
