f1802d994fbfe9b20a2aff77fbc26cb8e3f270ec
Bianca-Granata <biancag02@outlook.com>
2024-09-11 13:33:46 -0700

    started the introducing variables project

---
+++ topics/instructions/variables/introducing-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing Variables
+ * Bianca Granata
+ * 
+ * learning what a variable is and does
+ */
+
+"use strict";
+
+/**
+ * this function creates the canvas
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
