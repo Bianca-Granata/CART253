dc123041e130888347e5d8d2098bab6cdac8c981
Bianca-Granata <biancag02@outlook.com>
2024-09-13 14:01:25 -0700

    Used variables to make the program change

--- topics/instructions/variables/introducing-variables/js/script.js
+++ topics/instructions/variables/introducing-variables/js/script.js
@@ -11,13 +11,24 @@
  * this function creates the canvas
 */
 function setup() {
+    createCanvas(640, 480);
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws a circle in the center of the canvas
 */
 function draw() {
-
+    background(0);
+    //draw the circle
+    push();
+    fill(mouseX, mouseY, 0);
+    noStroke();
+    // by writing that the circle is also in the center
+    //ellipse(width / 2, height / 2, 100, 100)
+    /** makes the circle follow the mouse
+     * ellipse(mouseX, mouseY, 100, 100); */
+    ellipse(width / 2, height / 2, 100, 100);
+    pop();
 }
