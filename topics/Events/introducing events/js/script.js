7421d11ee9a879589e463abc860daf961b5a38e2
Bianca-Granata <biancag02@outlook.com>
2024-11-04 23:14:56 -0800

    made an event example

---
+++ topics/Events/introducing events/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * introducing events
+ * Author Name
+ * 
+ * taking a look at how events work in JavaScript and P5
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(400, 400);
+    background(0);
+}
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
+function mousePressed() {
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+    pop();
+}
