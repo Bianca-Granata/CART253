d1006632a2befc0fa9493a3015b4234ced555573
Bianca-Granata <biancag02@outlook.com>
2024-12-03 17:02:44 -0800

    finished speech speech

--- topics/Arrays/speech-speech/js/script.js
+++ topics/Arrays/speech-speech/js/script.js
@@ -7,18 +7,39 @@
  */
 
 "use strict";
-
+const speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+//whcih sentence in the speech to display
+let speechIndex = 0;
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600, 100);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * display the current line of speech
 */
 function draw() {
+    background(0);
 
+    let currentLine = speech[speechIndex];
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    //displaying in the center
+    text(currentLine, width / 2, height / 2);
+
+    pop();
+}
+//advances aour speech to the next line
+function mousePressed() {
+    //next line
+    speechIndex = speechIndex + 1;
+    if (speechIndex >= speech.length) {
+        //start over
+        speechIndex = 0;
+    }
 }
