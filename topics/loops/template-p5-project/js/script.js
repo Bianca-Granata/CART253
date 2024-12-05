1d621dc6ebde48f9145faa45b04bbdeeb8bbf435
Bianca-Granata <biancag02@outlook.com>
2024-12-04 16:37:16 -0800

    added only one food, intending to add more.

---
+++ topics/loops/template-p5-project/js/script.js
@@ -0,0 +1,44 @@
+/**
+ * draw a star-field wih for -loop
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+const numStars = 100;
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(400, 400);
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(0);
+
+    randomSeed(1);
+    for (let i = 0; i < numStars; i++) {
+        drawStar();
+    }
+}
+
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+
+    push();
+    fill(255);
+    noStroke();
+    ellipse(x, y, diameter);
+    pop();
+
+}
